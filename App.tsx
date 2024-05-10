310d5ef0649d2ade7cb373c811f191411c2facd5
Raphael Talento <129825828+RaphaelT1010@users.noreply.github.com>
2024-05-10 16:09:53 -0700

    GUI improvements
    (color for enable/disable and just spatial proximity stuff)

--- App.tsx
+++ App.tsx
@@ -91,7 +91,7 @@ const App = () => {
             </Text>
           </>
         ) : (
-          <Text style={styles.TitleText}>Please Connect to the Robot</Text>
+          <Text style={styles.TitleText}>Tractor Squad App</Text>
         )}
       </View>
       <View style={styles.inputContainer}>
@@ -147,9 +147,9 @@ const App = () => {
       </View>
       <TouchableOpacity
         onPress={connectedDevice ? disconnectFromDevice : openDeviceModal}
-        style={styles.ctaButton}
+        style={styles.connectButton}
       >
-        <Text style={styles.ctaButtonText}>
+        <Text style={styles.enableButtonText}>
           {connectedDevice ? "Disconnect" : "Connect"}
         </Text>
       </TouchableOpacity>
@@ -157,10 +157,10 @@ const App = () => {
         {robotToggle ? (
           <>
           <TouchableOpacity onPress={toggleRobot} style={styles.disableRobotButton}>
-          <Text style={styles.ctaButtonText}>Disable Robot</Text>
+          <Text style={styles.enableButtonText}>Enabled</Text>
           </TouchableOpacity></>
         ) : (
-          <Text style={styles.ctaButtonText}>Enable Robot</Text>
+          <Text style={styles.enableButtonText}>Disabled</Text>
         )}
         
       </TouchableOpacity>
@@ -187,11 +187,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f2f2f2",
+    position: "relative", // Set position to relative for positioning children
+
   },
   TitleWrapper: {
-    flex: 1,
+    position: "absolute", // Position the title absolutely
+    top: 40, // Adjust as needed to move the title up
+    left: 0, // Align title to the left edge
+    right: 0, // Align title to the right edge
     justifyContent: "center",
     alignItems: "center",
+
   },
   TitleText: {
     fontSize: 30,
@@ -201,12 +207,14 @@ const styles = StyleSheet.create({
     color: "black",
   },
   inputContainer: {
+    position: "absolute", // Position the input container absolutely
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "flex-start",
     marginHorizontal: 20,
-    marginTop: 10,
-    width: 275, // set a specific width
+    bottom: 150, // Adjust as needed to position parallel to arrows container
+    left: 90, // Adjust as needed to position parallel to arrows container
+    width: 275
   },
   input: {
     flex: 1,
@@ -219,9 +227,9 @@ const styles = StyleSheet.create({
     width: 30, // set a specific width
   },
   arrowsContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    position: "absolute",
+    bottom: 40,
+    right: 40 // Adjust as needed
   },
   row: {
     flexDirection: "row",
@@ -236,8 +244,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderRadius: 20,
     margin: 10,
-    width: 75,
-    height: 75,
+    width: 90,
+    height: 90,
   },
   arrowText: {
     color: "white",
@@ -252,7 +260,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginBottom: 10,
     borderRadius: 8,
-    width: 100, // Set width to a specific value
+    width: 100,
+    alignSelf: "center", // Align the button horizontally
+    position: "absolute", // Position the button absolutely
+    bottom: 20, // Adjust as needed to position it below the input container
+    left: "30%", // Center the button horizontally
+    marginLeft: -50, // Adjust to center the button
   },
   gpsButtonText: {
     fontSize: 18,
@@ -269,31 +282,52 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     width: 150, // Set width to a specific value
   },
+  enableButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 28.25, // Adjust the bottom margin to shift text down
+  },
   ctaButtonText: {
     fontSize: 18,
     fontWeight: "bold",
     color: "white",
   },
   enableRobotButton: {
+    backgroundColor: "#B53737", // Red
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    top: 100, // Shift the button down by 60 units
+    marginBottom: 10,
+    borderRadius: 8,
+    width: 125, // Set width to a specific value
+    position: "absolute", // Position the button absolutely
+    left: -62.5, // Shift the button half of its width to the left
+    transform: [{ rotate: "-90deg" }], // Rotate the button text
+  },
+  disableRobotButton: {
+    backgroundColor: "#0F9D58", //green
+  justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    marginBottom: 10,
+    borderRadius: 8,
+    width: 125, // Set width to a specific value
+    position: "absolute", // Position the button absolutely
+  },
+  connectButton: {
     backgroundColor: "#3065ba",
     justifyContent: "center",
     alignItems: "center",
     height: 60,
-    marginHorizontal: 20,
+    top: 250, // Shift the button down by 60 units
     marginBottom: 10,
     borderRadius: 8,
-    width: 150, // Set width to a specific value
-  },
-  disableRobotButton: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "#3065ba",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
+    width: 125, // Set width to a specific value
+    position: "absolute", // Position the button absolutely
+    left: -62.5, // Shift the button half of its width to the left
+    transform: [{ rotate: "-90deg" }], // Rotate the button text
   },
   disableButtonText: {
     fontSize: 18,
